6d8914c1eb66f6e6823a68925ffe47754c7b46fb
mpanczak <panczakmateusz@gmail.com>
2023-04-05 10:03:43 +0200

    formating changes

--- script.js
+++ script.js
@@ -2,165 +2,155 @@ const apikey = '5d066461-3cdb-4eaa-a460-793656e750c9';
 const apihost = 'https://todo-api.coderslab.pl';
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    apiListTasks().then(
+        function (response) {
 
-document.addEventListener('DOMContentLoaded', function() {
-  apiListTasks().then(
-    function(response) {
-
-      // "response" zawiera obiekt z kluczami "error" i "data" (zob. wyżej)
-      // "data" to tablica obiektów-zadań
-      // uruchamiamy funkcję renderTask dla każdego zadania jakie dał nam backend
-      response.data.forEach(
-        function(task) { renderTask(task.id, task.title, task.description, task.status); }
-      );
-
-    }
-  );  
+            // "response" zawiera obiekt z kluczami "error" i "data" (zob. wyżej)
+            // "data" to tablica obiektów-zadań
+            // uruchamiamy funkcję renderTask dla każdego zadania jakie dał nam backend
+            response.data.forEach(
+                function (task) {
+                    renderTask(task.id, task.title, task.description, task.status);
+                });
+        });
 });
-
 
 
 //List task
 function apiListTasks() {
-  return fetch(
-    apihost + '/api/tasks',
-    {
-      headers: { Authorization: apikey }
-    }
-  ).then(
-    function(resp) {
-      if(!resp.ok) {
-        alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
-      }
-      return resp.json();
-    }
-  )
+    return fetch(
+        apihost + '/api/tasks',
+        {
+            headers: {Authorization: apikey}
+        }).then(
+        function (resp) {
+            if (!resp.ok) {
+                alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
+            }
+            return resp.json();
+        });
 }
 
 //RenderTask
 function renderTask(taskId, title, description, status) {
-  const section = document.createElement('section');
-  section.className = 'card mt-5 shadow-sm';
-  document.querySelector('main').appendChild(section);
+    const section = document.createElement('section');
+    section.className = 'card mt-5 shadow-sm';
+    document.querySelector('main').appendChild(section);
 
-  const headerDiv = document.createElement('div');
-  headerDiv.className = 'card-header d-flex justify-content-between align-items-center';
-  section.appendChild(headerDiv);
+    const headerDiv = document.createElement('div');
+    headerDiv.className = 'card-header d-flex justify-content-between align-items-center';
+    section.appendChild(headerDiv);
 
-  const headerLeftDiv = document.createElement('div');
-  headerDiv.appendChild(headerLeftDiv);
+    const headerLeftDiv = document.createElement('div');
+    headerDiv.appendChild(headerLeftDiv);
 
-  const h5 = document.createElement('h5');
-  h5.innerText = title;
-  headerLeftDiv.appendChild(h5);
+    const h5 = document.createElement('h5');
+    h5.innerText = title;
+    headerLeftDiv.appendChild(h5);
 
-  const h6 = document.createElement('h6');
-  h6.className = 'card-subtitle text-muted';
-  h6.innerText = description;
-  headerLeftDiv.appendChild(h6);
+    const h6 = document.createElement('h6');
+    h6.className = 'card-subtitle text-muted';
+    h6.innerText = description;
+    headerLeftDiv.appendChild(h6);
 
-  const headerRightDiv = document.createElement('div');
-  headerDiv.appendChild(headerRightDiv);
+    const headerRightDiv = document.createElement('div');
+    headerDiv.appendChild(headerRightDiv);
 
-  if(status == 'open') {
-    const finishButton = document.createElement('button');
-    finishButton.className = 'btn btn-dark btn-sm js-task-open-only';
-    finishButton.innerText = 'Finish';
-    headerRightDiv.appendChild(finishButton);
-  }
+    if (status == 'open') {
+        const finishButton = document.createElement('button');
+        finishButton.className = 'btn btn-dark btn-sm js-task-open-only';
+        finishButton.innerText = 'Finish';
+        headerRightDiv.appendChild(finishButton);
+    }
 
-  const deleteButton = document.createElement('button');
-  deleteButton.className = 'btn btn-outline-danger btn-sm ml-2';
-  deleteButton.innerText = 'Delete';
-  headerRightDiv.appendChild(deleteButton);
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'btn btn-outline-danger btn-sm ml-2';
+    deleteButton.innerText = 'Delete';
+    headerRightDiv.appendChild(deleteButton);
 
-  const list = document.createElement('ul');
-  section.appendChild(list);
-
+    const list = document.createElement('ul');
+    section.appendChild(list);
 
 // "taskId" i "status" - bo tak nazywają się argumenty w funkcji "renderTask"
-apiListOperationsForTask(taskId).then(
-  function(response) {
-    response.data.forEach(
-      function(operation) { renderOperation(list, operation.id, status, operation.description, operation.timeSpent); }
-    );
-  }
-);
+    apiListOperationsForTask(taskId).then(
+        function (response) {
+            response.data.forEach(
+                function (operation) {
+                    renderOperation(list, operation.id, status, operation.description, operation.timeSpent);
+                });
+        });
 
-  const formDiv = document.createElement('div');
-  formDiv.className = 'cart-body';
-  section.appendChild(formDiv);
+    const formDiv = document.createElement('div');
+    formDiv.className = 'cart-body';
+    section.appendChild(formDiv);
 
-  const form = document.createElement('form');
-  formDiv.appendChild(form)
+    const form = document.createElement('form');
+    formDiv.appendChild(form)
 
-  const inputDiv = document.createElement('div');
-  inputDiv.className = 'input-group';
-  form.appendChild(inputDiv);
+    const inputDiv = document.createElement('div');
+    inputDiv.className = 'input-group';
+    form.appendChild(inputDiv);
 
-  const inputElement = document.createElement('input');
-  inputElement.className = 'form-control';
-  inputElement.setAttribute('type', 'text');
-  inputElement.setAttribute('placeholder', 'Operation description');
-  inputElement.setAttribute('minlength', '5');
-  inputDiv.appendChild(inputElement);
+    const inputElement = document.createElement('input');
+    inputElement.className = 'form-control';
+    inputElement.setAttribute('type', 'text');
+    inputElement.setAttribute('placeholder', 'Operation description');
+    inputElement.setAttribute('minlength', '5');
+    inputDiv.appendChild(inputElement);
 
-  const buttonDiv = document.createElement('div');
-  buttonDiv.className = 'input-group-append';
-  inputDiv.appendChild(buttonDiv);
+    const buttonDiv = document.createElement('div');
+    buttonDiv.className = 'input-group-append';
+    inputDiv.appendChild(buttonDiv);
 
-  const formButton = document.createElement('button');
-  formButton.classList.add('btn');
-  formButton.classList.add('btn-info');
-  formButton.innerText = 'Add';
-  buttonDiv.appendChild(formButton);
-  
+    const formButton = document.createElement('button');
+    formButton.classList.add('btn');
+    formButton.classList.add('btn-info');
+    formButton.innerText = 'Add';
+    buttonDiv.appendChild(formButton);
 
 
 }
 
 // List operations
 function apiListOperationsForTask(taskId) {
-  return fetch(
-    apihost + `/api/tasks/${taskId}/operations`,
-    {
-      headers: { Authorization: apikey }
-    }
-  ).then(
-    function(resp) {
-      if(!resp.ok) {
-        alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
-      }
-      return resp.json();
-    }
-  );
+    return fetch(
+        apihost + `/api/tasks/${taskId}/operations`,
+        {
+            headers: {Authorization: apikey}
+        }
+    ).then(
+        function (resp) {
+            if (!resp.ok) {
+                alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
+            }
+            return resp.json();
+        });
 }
 
 
 //Render Operations
 function renderOperation(operationsList, status, operationId, operationDescription, timeSpent) {
-  const li = document.createElement('li');
-  li.className = 'list-group-item d-flex justify-content-between align-items-center';
+    const li = document.createElement('li');
+    li.className = 'list-group-item d-flex justify-content-between align-items-center';
 
-  // operationsList to lista <ul>
-  operationsList.appendChild(li);
+    // operationsList to lista <ul>
+    operationsList.appendChild(li);
 
-  const descriptionDiv = document.createElement('div');
-  descriptionDiv.innerText = operationDescription;
-  li.appendChild(descriptionDiv);
+    const descriptionDiv = document.createElement('div');
+    descriptionDiv.innerText = operationDescription;
+    li.appendChild(descriptionDiv);
 
-  const time = document.createElement('span');
-  time.className = 'badge badge-success badge-pill ml-2';
-  time.innerText = timeSpent + 'm';
-  descriptionDiv.appendChild(time);
+    const time = document.createElement('span');
+    time.className = 'badge badge-success badge-pill ml-2';
+    time.innerText = timeSpent + 'm';
+    descriptionDiv.appendChild(time);
 
-  if(status == "open") {
-    //todo buttons ...
-  }
-  // ...
+    if (status == "open") {
+        //todo buttons ...
+    }
+    // ...
 }
-
-
 
 
 //Create
